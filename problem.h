f23dfaba1746ed1f29655e0a40fbca782146a97c
SelmanB <44111838+SelmanB@users.noreply.github.com>
2019-02-27 12:11:41 +0300

    Update problem.h

--- problem.h
+++ problem.h
@@ -20,10 +20,41 @@ public:
   double bestScore();       //IMPLEMENT,easy
   void printScores();       //IMPLEMENT,easy
   static size_t n=DEF_N;
-  vector<individual> population;
+  individual *population;
 };
 
 
+void killAndReplace(){
+    double t=0,cumSum[n],rnd;
+    int victim;
+  
+    for(int i=0;i<n;i++){    //find temperature(coefficient of distribution)
+        t+=population[i]->score;
+    }t/=n;
+  
+    for(int i=1;i<n;i++){    //assign all members a range proportional to the random weight
+      cumSum[i]=cumSum[i-1]+exp((population[i]->score)/t);
+    }
 
+    rnd=gnrandf(cumSum[n-1]);//generate a random number in the whole range
+                          
+     //find whose interval is it in (I'll do first in O(n) then someone may write binary search(O(logn)))
+  for(int i=0; i<n; i++){
+       if(rnd<cumSum[i]){
+            victim=i;
+            break;
+       }
+  }
 
+      //Choose mom and dad
+  int a=victim;
+  while(a==victim) a=gnrandi(n);
+  
+  int b=victim;
+  while(b==victim) b=gnrandi(n);
+  
+      //Replace victim with a child of lucky mom and dad
+  delete population[victim];
+  population[victim]=new individual(*population[a],population[b]);
+}
 #endif //GENOOB_PROBLEM_H
