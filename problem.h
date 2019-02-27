a5e65528450afe237f8b5d0b1243cdd68deb786e
SelmanB <44111838+SelmanB@users.noreply.github.com>
2019-02-27 11:46:25 +0300

    Update problem.h

--- problem.h
+++ problem.h
@@ -6,13 +6,18 @@
 #define GENOOB_PROBLEM_H
 
 #include "individual.h"
+#include <vector.h>
+using namespace std;
+
+#define DEF_N 100
 
 class problem{
-
-
-
-
-
+public:
+  problem(size_t popSize=DEF_N, size_t genSize=DEF_GENSIZE, genType _type=DEF_TYPE);
+  void killAndReplace();    //IMPLEMENT, CHOOSES WHO TO KILL AND FILLS THE SPACE BY BREEDING OTHER INDIVIDUALS USING BREEDER CONSTRUCTER
+  
+  static size_t n=DEF_N;
+  vector<individual> population;
 };
 
 
