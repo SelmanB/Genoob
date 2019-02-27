b75cc6e2073189ca232583e4c0c854390ebc7e44
ArdaBar <47534165+ArdaBar@users.noreply.github.com>
2019-02-27 22:45:45 +0300

    Update individual.h

--- individual.h
+++ individual.h
@@ -59,6 +59,14 @@ individual::individual(const void *data) {
     score=eval();
 }
 
+double individual::eval(){
+  int count=0;
+    for(int i=0; i<size_t; i++)
+    {
+      count+=(binary[i]);
+    }
+    return count;
+}
 
 individual::mutate(){
     switch(type){
